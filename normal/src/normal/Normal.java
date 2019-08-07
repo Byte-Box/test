c238d72403878555be0cc9ace4c36c53462e0559
Sabyasachi Chakraborty <sabya.chakz@gmail.com>
2019-08-07 20:04:11 +0530

    this is my normal code

---
+++ normal/src/normal/Normal.java
@@ -0,0 +1,18 @@
+
+package normal;
+
+/**
+ *
+ * @author KIIT
+ */
+public class Normal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("All ok");
+    }
+    
+}
